6f53f262c4203ab8c26d249c1c2f216260ec5f5b
Andrew <andrew.casin@gmail.com>
2020-11-13 10:23:33 -0600

    commit tree 5f6fa12d20b8694b3cafda92aa2f158bf005dafc

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,3 +1,31 @@
 class BankAccount
+    attr_reader :name
+    attr_accessor :balance, :status
+
+    def initialize(name)
+        @name = name
+        @balance = 1000 #always start with 1000 balance
+        @status = "open" #always start with status of 'open'
+    end
+
+    def deposit(money)
+        @balance += money
+    end
+
+    def display_balance
+        "Your balance is $#{self.balance}."
+    end
+
+    def valid?
+        if @status == "open" && @balance > 0
+            true
+        else
+            false
+        end
+    end
+
+    def close_account
+        @status = "closed"
+    end
 
 end
--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,43 @@
 class Transfer
   # your code here
+  attr_accessor :sender, :receiver, :status, :amount
+
+  def initialize(sender, receiver, amount)
+    @sender = sender
+    @receiver = receiver
+    @amount = amount
+    @status = "pending" 
+  end
+
+  def valid?
+    # @sender.BankAccount.valid? ? true : false
+    if @sender.valid? == true && @receiver.valid? == true
+      true
+    else
+      false
+    end
+  end
+
+  def execute_transaction #want to subtract amount from sender's balance, and add amount to receiver's balance
+    if valid? && sender.balance > amount && self.status == "pending"
+      sender.balance -= amount
+      receiver.balance += amount
+      self.status = "complete"
+    else
+      self.status = "rejected"
+      "Transaction rejected. Please check your account balance."
+    end
+  end
+
+  def reverse_transfer #reverse a completed transfer
+    if self.status = "complete" && execute_transaction #and execute transaction is true
+      sender.balance += amount
+      receiver.balance -= amount
+      self.status = "reversed"
+    end
+    
+  end
+
+
+
 end
